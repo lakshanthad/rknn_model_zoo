50cf1207f2d01b3eeffa30be6e69ee2611da7188
Lakshantha <lakshantha@ultralytics.com>
2024-08-23 09:35:28 -0700

    Revert "Remove extra post_process"
    This reverts commit d60527fa0be9d3d1db8dd4b8d267ee45e9f71585.

--- examples/yolov8/python/yolov8.py
+++ examples/yolov8/python/yolov8.py
@@ -85,16 +85,83 @@ def nms_boxes(boxes, scores):
     keep = np.array(keep)
     return keep
 
-def post_process(input_data):
-    """Process model outputs."""
-    boxes, scores, class_probs = input_data[:3]
-    boxes, classes, scores = filter_boxes(boxes, scores, class_probs)
+def dfl(position):
+    # Distribution Focal Loss (DFL)
+    import torch
+    x = torch.tensor(position)
+    n,c,h,w = x.shape
+    p_num = 4
+    mc = c//p_num
+    y = x.reshape(n,p_num,mc,h,w)
+    y = y.softmax(2)
+    acc_metrix = torch.tensor(range(mc)).float().reshape(1,1,mc,1,1)
+    y = (y*acc_metrix).sum(2)
+    return y.numpy()
 
-    if len(boxes) == 0:
+
+def box_process(position):
+    grid_h, grid_w = position.shape[2:4]
+    col, row = np.meshgrid(np.arange(0, grid_w), np.arange(0, grid_h))
+    col = col.reshape(1, 1, grid_h, grid_w)
+    row = row.reshape(1, 1, grid_h, grid_w)
+    grid = np.concatenate((col, row), axis=1)
+    stride = np.array([IMG_SIZE[1]//grid_h, IMG_SIZE[0]//grid_w]).reshape(1,2,1,1)
+
+    position = dfl(position)
+    box_xy  = grid +0.5 -position[:,0:2,:,:]
+    box_xy2 = grid +0.5 +position[:,2:4,:,:]
+    xyxy = np.concatenate((box_xy*stride, box_xy2*stride), axis=1)
+
+    return xyxy
+
+def post_process(input_data):
+    boxes, scores, classes_conf = [], [], []
+    defualt_branch=3
+    pair_per_branch = len(input_data)//defualt_branch
+    # Python 忽略 score_sum 输出
+    for i in range(defualt_branch):
+        boxes.append(box_process(input_data[pair_per_branch*i]))
+        classes_conf.append(input_data[pair_per_branch*i+1])
+        scores.append(np.ones_like(input_data[pair_per_branch*i+1][:,:1,:,:], dtype=np.float32))
+
+    def sp_flatten(_in):
+        ch = _in.shape[1]
+        _in = _in.transpose(0,2,3,1)
+        return _in.reshape(-1, ch)
+
+    boxes = [sp_flatten(_v) for _v in boxes]
+    classes_conf = [sp_flatten(_v) for _v in classes_conf]
+    scores = [sp_flatten(_v) for _v in scores]
+
+    boxes = np.concatenate(boxes)
+    classes_conf = np.concatenate(classes_conf)
+    scores = np.concatenate(scores)
+
+    # filter according to threshold
+    boxes, classes, scores = filter_boxes(boxes, scores, classes_conf)
+
+    # nms
+    nboxes, nclasses, nscores = [], [], []
+    for c in set(classes):
+        inds = np.where(classes == c)
+        b = boxes[inds]
+        c = classes[inds]
+        s = scores[inds]
+        keep = nms_boxes(b, s)
+
+        if len(keep) != 0:
+            nboxes.append(b[keep])
+            nclasses.append(c[keep])
+            nscores.append(s[keep])
+
+    if not nclasses and not nscores:
         return None, None, None
 
-    keep_indices = nms_boxes(boxes, scores)
-    return boxes[keep_indices], classes[keep_indices], scores[keep_indices]
+    boxes = np.concatenate(nboxes)
+    classes = np.concatenate(nclasses)
+    scores = np.concatenate(nscores)
+
+    return boxes, classes, scores
 
 
 def draw(image, boxes, scores, classes):
